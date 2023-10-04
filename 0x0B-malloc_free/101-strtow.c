8f049e5387bfa4954d3277570da65d9151e72a30
brianshaddi <briangitahi0@gmail.com>
2023-10-04 15:34:42 -0700

    last

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,5 +1,12 @@
 #include <stdlib.h>
 #include "main.h"
+
+/**
+ * count_word - helper function to count the number of words in a string
+ * @s: string to evaluate
+ *
+ * Return: number of words
+ */
 int count_word(char *s)
 {
 	int flag, c, w;
